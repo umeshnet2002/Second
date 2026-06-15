d44a7e62159c123d02b8be68becdecebbabf6a28
Umesh Das <dasumesh2002@gmail.com>
2026-06-15 18:20:35 +0530

    data

--- New_Test/src/test/java/dataproviders/TestDataProviders.java
+++ New_Test/src/test/java/dataproviders/TestDataProviders.java
@@ -5,7 +5,7 @@ import org.testng.annotations.DataProvider;
 public class TestDataProviders {
 	@DataProvider(name = "RegistrationData")
 	public Object[][] getRegistrationData() {
-		return new Object[][] { { "John", "Doe", "doejohn123456@gmail.com","12894967", "password123" }
+		return new Object[][] { { "John", "Doe", "doejohn1234567@gmail.com","12894967", "password123" }
 
 		};
 	}
